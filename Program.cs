433725858d8ed79ebf9c683e0cba3b29fefcdfc9
JPierre7 <pierleo1997@hotmail.com>
2024-10-01 05:13:20 +0000

    Create endpoints for crud inmueble

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
 using dao.Connection;
+using Inmuebles.DTO;
 using Inmuebles.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-if(connectionString is null)
-    throw new Exception("Connection string not found");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new Exception("Connection string not found");
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -33,7 +32,7 @@ app.MapGet("/", (MsSQLConnection connection) =>
 .WithName("Root")
 .WithOpenApi();
 
-app.MapPost("/inmueble", ([FromBody] Inmueble inmueble, MsSQLConnection connection) => {
+app.MapPost("/inmueble", ([FromBody] InmuebleDTO inmueble, MsSQLConnection connection) => {
     var parameters = new Dictionary<string, object>
     {
         { "direccion", inmueble.Direccion },
@@ -41,22 +40,38 @@ app.MapPost("/inmueble", ([FromBody] Inmueble inmueble, MsSQLConnection connecti
         { "estadoInmueble", inmueble.EstadoInmueble },
         { "disponibilidad", inmueble.Disponibilidad },
         { "ciudad", inmueble.Ciudad },
-        { "tipoInmueble", 1 }
+        { "tipoInmuebleID", inmueble.TipoInmuebleID }
     };
 
-    return connection.GetOne<Inmueble>("dbo.SP_INSERT_INMUEBLE", parameters);;
+    return connection.GetOne<Inmueble>("dbo.SP_INSERT_INMUEBLE", parameters);
 })
 .WithName("Root2")
 .WithOpenApi();
 
-app.MapPut("/inmueble/{id}", (string id) => {
-    return "Hello world";
+app.MapPut("/inmueble/{id}", (int id, [FromBody] InmuebleDTO inmueble, MsSQLConnection connection) => {
+    var parameters = new Dictionary<string, object>
+    {
+        { "idInmueble", id },
+        { "direccion", inmueble.Direccion },
+        { "cantHabitaciones", inmueble.CantidadHabitaciones },
+        { "estadoInmueble", inmueble.EstadoInmueble },
+        { "disponibilidad", inmueble.Disponibilidad },
+        { "ciudad", inmueble.Ciudad },
+        { "tipoInmuebleID", inmueble.TipoInmuebleID }
+    };
+
+    return connection.GetOne<Inmueble>("dbo.SP_UPDATE_INMUEBLE", parameters);
 })
 .WithName("Root3")
 .WithOpenApi();
 
-app.MapDelete("/inmueble/{id}", (string id) => {
-    return "Hello world";
+app.MapDelete("/inmueble/{id}", (int id, MsSQLConnection connection) => {
+    var parameters = new Dictionary<string, object>
+    {
+        { "idInmueble", id }
+    };
+
+    return connection.Confirm("dbo.SP_DELETE_INMUEBLE", parameters);
 })
 .WithName("Root4")
 .WithOpenApi();
